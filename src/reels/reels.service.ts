c1a83014b074cfd2d1ff90903d7039bba06b4d21
jed lahrim <111662171+jedLahrim@users.noreply.github.com>
2025-01-30 20:00:41 +0000

    Update reels.service.ts

--- src/reels/reels.service.ts
+++ src/reels/reels.service.ts
@@ -68,7 +68,8 @@ export class ReelsService {
    * Cron job to fetch and save reels daily at midnight
    */
   async scheduledFetchReels(): Promise<{ success: string }> {
-    console.log('Running scheduled job to fetch reels...');
+    cron.schedule('15 20 * * *', async () => {
+      console.log('Running scheduled job to fetch reels...');
 
       for (const username of Constant.usernames) {
         // Get the stored pagination token for this username
@@ -87,6 +88,7 @@ export class ReelsService {
       }
 
       console.log('Finished fetching reels.');
+    });
     return {success:'reels fetching Scheduled successfully'}
   }
 
