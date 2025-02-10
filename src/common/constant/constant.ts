44718795a07f7649080a343e623d1737223f32e9
jed lahrim <111662171+jedLahrim@users.noreply.github.com>
2025-02-10 22:40:40 +0000

    Update constant.ts

--- src/common/constant/constant.ts
+++ src/common/constant/constant.ts
@@ -2,9 +2,9 @@ export class Constant {
   static OPEN_AI_URL =
     'https://chatgpt-best-price.p.rapidapi.com/v1/chat/completions';
   static OPEN_AI_HEADERS = {
-    'x-rapidapi-key': 'c9a76cdc95msh9e96e12f2102067p1654bajsnd47e95803a41',
+    'x-rapidapi-key': '4fb84e5862msh93493191641aa67p1730ebjsn9a38755eabe6',
     'x-rapidapi-host': 'chatgpt-best-price.p.rapidapi.com',
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/json'
   };
 
   static readonly usernames = ['pets','Pet','jiffpom', 'nala_cat', 'realgrumpycat', 
