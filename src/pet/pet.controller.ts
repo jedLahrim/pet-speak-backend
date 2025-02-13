6145b98fa16649318eb0186eee9a199dcbf210dc
jed lahrim <111662171+jedLahrim@users.noreply.github.com>
2025-02-13 20:41:20 +0000

    Update pet.controller.ts

--- src/pet/pet.controller.ts
+++ src/pet/pet.controller.ts
@@ -128,7 +128,7 @@ export class PetController {
     return this.petService.chat(dto);
   }
 
-  @Get('generate/quiz')
+  @Post('generate/quiz')
   @UseGuards(JwtAuthGuard)
   getQuiz(@Body() dto: { petType: PetType }) {
     return this.petService.getQuiz(dto);
