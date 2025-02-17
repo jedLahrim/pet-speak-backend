340af1ba6bc23e2a4fc2db92f63a645f5b2c017d
jed lahrim <111662171+jedLahrim@users.noreply.github.com>
2025-02-17 13:54:57 +0000

    Update constant.ts

--- src/common/constant/constant.ts
+++ src/common/constant/constant.ts
@@ -1,10 +1,8 @@
 export class Constant {
   static OPEN_AI_URL =
-    'https://chatgpt-best-price.p.rapidapi.com/v1/chat/completions';
+    'https://free-chat-gpt-umber.vercel.app/api/chat';
   static OPEN_AI_HEADERS = {
-    'x-rapidapi-key': '6789eae184msh516c6842cc65df5p16bcffjsn62cda002e24c',
-    'x-rapidapi-host': 'chatgpt-best-price.p.rapidapi.com',
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
   };
 
   static readonly usernames = ['pets','Pet','jiffpom', 'nala_cat', 'realgrumpycat', 
