dc8d508fe573b479c23fe887128f0346f8d93151
jed lahrim <111662171+jedLahrim@users.noreply.github.com>
2025-02-19 21:43:13 +0000

    Create reels.entity.ts

---
+++ src/pet/entity/reels.entity.ts
@@ -0,0 +1,14 @@
+export interface Reel {
+  id?: string;
+  displayUrl?: string;
+  videoUrl?: string;
+  caption?: string;
+  ownerFullName?: string;
+  ownerUsername?: string;
+  url?: string;
+  key?: string;
+  commentsCount?: number;
+  firstComment?: string;
+  likesCount?: number;
+  timestamp?: Date;
+}
