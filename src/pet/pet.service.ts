eb21229292bc61959757b8b8842da57bb21f589f
jed lahrim <111662171+jedLahrim@users.noreply.github.com>
2025-03-13 22:26:29 +0000

    Update pet.service.ts

--- src/pet/pet.service.ts
+++ src/pet/pet.service.ts
@@ -1,4 +1,7 @@
-import { Injectable, NotFoundException, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, NotFoundException, 
+        InternalServerErrorException, 
+        BadRequestException 
+       } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Pet } from './entity/pet.entity';
