fb1d81c34aea3c48b6bffa865b358030271f1bfc
jed lahrim <111662171+jedLahrim@users.noreply.github.com>
2025-04-07 19:10:43 +0100

    Update pet.service.ts

--- src/pet/pet.service.ts
+++ src/pet/pet.service.ts
@@ -185,7 +185,7 @@ async transcribeAudio(audioFile: Express.Multer.File) {
     languageCode: string,
   ): Promise<string> {
     try {
-      const prompt = `Please Generates a full text rewriting the following text with a clear and concise explanation in the same language code ${languageCode}. Ensure the generated text is not less than 300 characters. Do not exceed or fall short of this range. 
+      const prompt = `Please Generates a full text rewriting the following text with a clear and concise explanation in the same language code ${languageCode}. Ensure the generated text is detailed. Do not exceed or fall short of this range. 
               Here is the text: ${originalText}`;
       const data = await this._callAi(prompt);
       return data?.choices[0].message?.content;
